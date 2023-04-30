4991ab3873818a3fbaa5778468618e8560b4c94f
MarufSourav <maruf.sourav99@gmail.com>
2023-04-30 18:41:26 -0400

    Quit on Death Screen

--- GroupProject3/Source/Script/DeathScene.gd
+++ GroupProject3/Source/Script/DeathScene.gd
@@ -2,3 +2,5 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_just_pressed("restart"):
 		get_tree().change_scene_to_file("res://Scene/World.tscn")
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
